a60c799d24a39a95d3df4eca34a5ac4f348e94c1
constbitt <ms.biderina@mail.ru>
2025-06-18 00:16:38 +0200

    added No Sql injection tests

---
+++ src/test/java/vulnerabilities/NoSqlInjectionTests.java
@@ -0,0 +1,52 @@
+package vulnerabilities;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+
+public class NoSqlInjectionTests extends TestBase {
+
+    @Override
+    protected String getEndpoint() {
+        return "/no-sql-injection";
+    }
+
+    @Test
+    void testUsernameNeNull(TestInfo testInfo) {
+        testLogin(testInfo, "{\"$ne\": null}", "admin");
+    }
+
+    @Test
+    void testPasswordNeNull(TestInfo testInfo) {
+        testLogin(testInfo, "admin", "{\"$ne\": null}");
+    }
+
+    @Test
+    void testPasswordRegex(TestInfo testInfo) {
+        testLogin(testInfo, "admin", "{\"$regex\": \".*\"}");
+    }
+
+    @Test
+    void testUsernameRegex(TestInfo testInfo) {
+        testLogin(testInfo, "{\"$regex\": \".*\"}", "admin");
+    }
+
+    @Test
+    void testUsernameGtEmpty(TestInfo testInfo) {
+        testLogin(testInfo, "{\"$gt\": \"\"}", "admin");
+    }
+
+    @Test
+    void testPasswordGtEmpty(TestInfo testInfo) {
+        testLogin(testInfo, "admin", "{\"$gt\": \"\"}");
+    }
+
+    @Test
+    void testUsernameNeAdmin(TestInfo testInfo) {
+        testLogin(testInfo, "{\"$ne\": \"admin\"}", "admin");
+    }
+
+    @Test
+    void testPasswordNeAdmin(TestInfo testInfo) {
+        testLogin(testInfo, "admin", "{\"$ne\": \"admin\"}");
+    }
+}
